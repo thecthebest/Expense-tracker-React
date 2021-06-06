da7512f08c3d6c2aafd30f2331148672ea065a83
alis <asyed5839@gmail.com>
2021-06-06 11:54:14 +0100

    Made changes to components references
    -App.js: Expenses.js reference was updated and a new reference to
    NewExpense.js was added.
    -ExpenseItem.js: Card.js reference was updated.
    -Expenses.js: Card.js reference was updated.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 function App() {
     const expenses = [
         {
@@ -23,7 +24,7 @@ function App() {
       ];
     return (
         <div>
-            <h1>My React App</h1>
+            <NewExpense></NewExpense>
             <Expenses items={expenses}/>
         </div>
     );
