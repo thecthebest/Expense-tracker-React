f53296ad881222fe12f692332baf15d277bdeb5a
alis <asyed5839@gmail.com>
2021-06-07 11:10:11 +0100

    Changes made to allow props between parent & child
    The file affected was Expenses.js. A new useState hook was added and a
    new custom component was added to JSX code. The component include
    pointer to parent components and also receieve props from the parent.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,20 @@
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import { useState } from 'react';
 function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = (selectedYear) => {
+        console.log(selectedYear);
+        setFilteredYear(selectedYear);
+    };
+
     return (
         <Card className="expenses">
+            <div>
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
+            </div>
             <ExpenseItem title={props.items[0].title} date={props.items[0].date} amount={props.items[0].amount} />
             <ExpenseItem title={props.items[1].title} date={props.items[1].date} amount={props.items[1].amount} />
             <ExpenseItem title={props.items[2].title} date={props.items[2].date} amount={props.items[2].amount} />
