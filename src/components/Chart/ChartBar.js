a18d0648506d01d75bfae67d779c8442d12afbcb
alis <asyed5839@gmail.com>
2021-06-08 15:57:25 +0100

    Added functionality and styles to the components
    File affected were:
    Chart.css
    Chart.js
    ChartBar.css
    ChartBar.js

---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,19 @@
+import './Chart.css';
+function Chart(props) {
+    let barFillHeight= '0%';
+    if (props.max > 0) {
+        barFillHeight = 
+            Math.round((props.vale / props.maxValue) * 100) + '%';
+    }
+
+
+    return (
+        <div className="chart-bar">
+            <div className="chart-bar__inner">
+                <div className="chart-bar__fill" style={{height: barFillHeight}}></div>
+            </div>
+            <div className="chart-bar__label">{props.label}</div>
+        </div>
+    );
+}
+export default Chart;
